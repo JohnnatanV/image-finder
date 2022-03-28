fc09ea1d5ed50d96d111922cee71d6a7e611c5b9
Johnnatan Villaneda <joh.villaneda@outlook.com>
2022-03-28 09:25:50 -0500

    Update files for App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import "./App.css";
+import "./css/Header.css";
+import "./css/Content.css";
+import "./css/Article.css";
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = (url) => window.open(url);
+  console.log({ photos });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            const reponse = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=10&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID 7D-0T9Nvc54PGNCo2U7WBsz57iDRxcTfO_SoDlzmmQw",
+                },
+              }
+            );
+            const data = await reponse.json();
+            //llamar a unsplash
+            setPhotos(data.results);
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.alt_description} />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
